159e7b7417c68df64cc9e3d8737d7200cd2ad56c
silas0802 <40304986+silas0802@users.noreply.github.com>
2024-02-10 23:20:50 +0100

    ip felt

--- Magical Mayhem/Assets/Scripts/UI/ConnectionHUD.cs
+++ Magical Mayhem/Assets/Scripts/UI/ConnectionHUD.cs
@@ -1,14 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.Netcode;
 using Unity.Netcode.Transports.UTP;
 using UnityEngine;
 using UnityEngine.Networking;
 
-public class JoinButton : MonoBehaviour
+public class ConnectionHUD : MonoBehaviour
 {
+    public TMP_InputField inputField;
     const string LOCALHOST = "127.0.0.1"; 
-    const string IP = "192.168.3.107";
     public void JoinGame(){
         SetConnectionData();
         NetworkManager.Singleton.StartClient();
@@ -20,8 +21,14 @@ public class JoinButton : MonoBehaviour
 
     public void SetConnectionData()
     {
+        string input = inputField.text;
+        if (input == null || input == "")
+        {
+            print("Lockal");
+            input = LOCALHOST;
+        }
         NetworkManager.Singleton.GetComponent<UnityTransport>().SetConnectionData(
-            IP,  // The IP address is a string
+            input,  // The IP address is a string
             12345, // The port number is an unsigned short
             "0.0.0.0" // The server listen address is a string. 0.0.0.0 makes it listen for everything
         );
