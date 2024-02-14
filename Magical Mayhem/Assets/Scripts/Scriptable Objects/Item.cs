f8d9262944fc7f3fe4ff412b9d5ad8a6b43d9126
silas0802 <silasthule@gmail.com>
2024-02-14 16:00:16 +0100

    buyable

--- Magical Mayhem/Assets/Scripts/Scriptable Objects/Item.cs
+++ Magical Mayhem/Assets/Scripts/Scriptable Objects/Item.cs
@@ -3,15 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "New Item", menuName = "Game/Item")]
-public class Item : ScriptableObject
+public class Item : Buyable
 {
-    [SerializeField] private string Description;
-    [SerializeField] private Sprite Icon;
-    [SerializeField] private int Price;
-
-    public string description => this.Description;
-    public Sprite icon => this.Icon;
-    public int price => this.Price;
+  
 
     
 }
--- Magical Mayhem/Assets/Scripts/Scriptable Objects/Spell.cs
+++ Magical Mayhem/Assets/Scripts/Scriptable Objects/Spell.cs
@@ -2,14 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Spell : ScriptableObject
+public abstract class Spell : Buyable
 {
-    [TextArea(5,10)] public string Description = "This is a spell";
 
     public SpellType Type;
 
-    [SerializeField,Range(1,20), Tooltip("The amount of gold required to purchase this spell")] 
-    private int Price = 10;
 
     [Range(0,20), Tooltip("The duration you have to wait before, you can use the spell again")] 
     private float Cooldown = 3;
@@ -17,7 +14,6 @@ public abstract class Spell : ScriptableObject
     [Range(0,3), Tooltip("The amount of time in between the use of a spell and the spells effect taking place.")] 
     private float CastTime = 0.5f;
 
-    public int price => this.Price;
     public float cooldown => this.Cooldown;
     public float castTime => this.CastTime;
 
