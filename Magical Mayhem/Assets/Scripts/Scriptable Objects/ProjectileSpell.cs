f35e505865986a88b4ceac28f035ccc7ab5c93ac
silas0802 <silasthule@gmail.com>
2024-02-16 08:08:55 +0100

    fireball

--- Magical Mayhem/Assets/Scripts/Scriptable Objects/ProjectileSpell.cs
+++ Magical Mayhem/Assets/Scripts/Scriptable Objects/ProjectileSpell.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/// <summary>
+/// - Silas Thule
+/// </summary>
 [CreateAssetMenu(fileName = "New Projectile Spell", menuName = "Game/Spells/Projectile Spell")]
 
 public class ProjectileSpell : Spell
 {
     [SerializeField,Range(1,30),Tooltip("How fast the projectile moves")] private float Speed = 10;
-
     [SerializeField,Range(0,30), Tooltip("How far the projectile can travel")] private float Range = 10;
     [SerializeField,Range(0,3), Tooltip("How far away from the projectile a target can be before it explodes")] private float TriggerRadius = 0.5f;
     [SerializeField] private ProjectileInstance Projectile;
     [SerializeField, Tooltip("What effect that happens when the projectile hits")] private ExplosionSpell EndEffect;
-    //Jesper sutter diller
     public float speed => this.Speed;
     public float range => this.Range;
     public float triggerRadius => this.TriggerRadius;
