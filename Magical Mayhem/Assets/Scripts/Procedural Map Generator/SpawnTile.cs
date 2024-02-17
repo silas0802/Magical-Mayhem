5d38ecdec432b50ae0fd718db3b4d2ff2aad7742
Hvogu <92995572+Hvogu@users.noreply.github.com>
2024-02-17 15:28:55 +0100

    map V0.0.2
    usynlige walls rundt om det genererede map plus lidt ekstra

--- Magical Mayhem/Assets/Scripts/Procedural Map Generator/SpawnTile.cs
+++ Magical Mayhem/Assets/Scripts/Procedural Map Generator/SpawnTile.cs
@@ -1,19 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using Unity.Mathematics;
+using UnityEditor.ShaderGraph;
 using UnityEngine;
 
 public class SpawnTile : MonoBehaviour
 {
     [SerializeField] private TileScript floor;
     [SerializeField] private WallScript wall;
-    int mapSize = 20;
-    TileScript[,] tilearray;
+    
+    int mapSize = 41;
+    int lavaTileCounter = 0;
+    float wallHieght = 5f;
+    float lavaSpawnTime = 10f;
+    float nextLavaSpawn;
+    TileScript[,] tileArray;
     
     // Start is called before the first frame update
     void Start()
-    {
-        tilearray = new TileScript[mapSize,mapSize];
+    {   
+        //set the time until map gets smaller
+        nextLavaSpawn = Time.time + nextLavaSpawn;
+        //save the floortiles
+        tileArray = new TileScript[mapSize,mapSize];
         for (int i = 0; i < mapSize; i++)
         {
             for (int j = 0; j < mapSize; j++)
@@ -21,10 +31,11 @@ public class SpawnTile : MonoBehaviour
                 TileSpawner(j, i);
             }
         }
-        WallScript southWall = Instantiate(wall, new Vector3(0, 0, 0), quaternion.identity);
-        WallScript northWall = Instantiate(wall, new Vector3(mapSize-1, 0, mapSize-1), quaternion.identity);
-        WallScript eastWall = Instantiate(wall, new Vector3(mapSize-1, 0, 0), quaternion.identity);
-        WallScript westWall = Instantiate(wall, new Vector3(0, 0, mapSize-1), quaternion.identity);
+        //init borderwalls
+        WallScript southWall = Instantiate(wall, new Vector3(mapSize/2, wallHieght/2, 0), quaternion.identity);
+        WallScript northWall = Instantiate(wall, new Vector3(mapSize/2, wallHieght/2, mapSize-1), quaternion.identity);
+        WallScript eastWall = Instantiate(wall, new Vector3(mapSize-1, wallHieght/2, mapSize/2), quaternion.identity);
+        WallScript westWall = Instantiate(wall, new Vector3(0, wallHieght/2, mapSize/2), quaternion.identity);
         SetWalls(southWall, northWall, eastWall, westWall);
 
     }
@@ -32,18 +43,34 @@ public class SpawnTile : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        lava(tileArray);
     }
 
     //This function transforms the walls to fit the egde of the gamemap and stops the player from falling off
     void SetWalls(WallScript southWall, WallScript northWall, WallScript eastWall, WallScript westWall){
-        southWall.transform.localScale = new Vector3(mapSize,1,1);
-        northWall.transform.localScale = new Vector3(mapSize,1,1);
-        //eastWall.transform.localScale = new Vector3(1,)
+        southWall.transform.localScale = new Vector3(mapSize,wallHieght,1);
+        northWall.transform.localScale = new Vector3(mapSize,wallHieght,1);
+        eastWall.transform.localScale = new Vector3(1,wallHieght,mapSize);
+        westWall.transform.localScale = new Vector3(1,wallHieght,mapSize);
+
     }
 
     //instanciates the floortiles and saves them in an array
     void TileSpawner(int xCoord, int yCoord){
-        tilearray[xCoord,yCoord] = Instantiate(floor, new Vector3(xCoord, 0, yCoord), quaternion.identity);
+        tileArray[xCoord,yCoord] = Instantiate(floor, new Vector3(xCoord, 0, yCoord), quaternion.identity);
+    }
+
+    void lava(TileScript[,] map){
+        if (Time.time > nextLavaSpawn){
+            for (int i = lavaTileCounter; i < mapSize-lavaTileCounter; i++)
+            {
+                // map[i,0] = set to now be lava tile;
+                // map[0,i]    
+                // map[mapSize-1,i]
+                // map[i,mapSize-1]
+            }
+            nextLavaSpawn = Time.time + lavaSpawnTime;
+            lavaTileCounter++;
+        }
     }
 }
