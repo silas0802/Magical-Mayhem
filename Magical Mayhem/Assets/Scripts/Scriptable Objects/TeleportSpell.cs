13361836b98ee9d3780bf21652f18ec9cf6b72d4
silas0802 <silasthule@gmail.com>
2024-04-11 12:51:58 +0200

    Teleport spell

--- Magical Mayhem/Assets/Scripts/Scriptable Objects/TeleportSpell.cs
+++ Magical Mayhem/Assets/Scripts/Scriptable Objects/TeleportSpell.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "New Teleport Spell", menuName = "Game/Spells/Teleport Spell")]
@@ -7,12 +8,23 @@ using UnityEngine;
 public class TeleportSpell : Spell
 {
     [SerializeField,Range(0,30)] private float Range = 10f;
-    [SerializeField] private GameObject TeleportVFX;
+    [SerializeField] private NetworkObject TeleportVFX;
 
     public float range => this.Range;
-    public GameObject teleportVFX => this.TeleportVFX;
+    public NetworkObject teleportVFX => this.TeleportVFX;
     public override void Activate(UnitController owner, Vector3 target)
     {
-        throw new System.NotImplementedException();
+        NetworkObject n1 = Instantiate(teleportVFX, owner.transform.position, Quaternion.identity);
+        NetworkObject n2 = Instantiate(teleportVFX, target, Quaternion.identity);
+        n1.Spawn();
+        n2.Spawn();
+        Vector3 distanceVec = target - owner.transform.position;
+        if (distanceVec.magnitude > range){
+            owner.transform.position = owner.transform.position + distanceVec.normalized*range;
+        }
+        else{
+            owner.transform.position = owner.transform.position + distanceVec;
+        }
+        owner.transform.position = target;
     }
 }
