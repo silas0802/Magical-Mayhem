c97c12c80659717646d07a85bb775a8e7cb89f56
jesperbesper <jesper.p03@gmail.com>
2024-04-12 09:50:44 +0200

    dashspell working

---
+++ Magical Mayhem/Assets/Scripts/Instances/DashInstance.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class DashInstance : NetworkBehaviour
+{
+     DashSpell dashSpell;
+     UnitController owner;
+     Vector3 target;
+     Vector3 origin;
+     Vector3 direction;
+    public void Initialize(DashSpell dashSpell,UnitController owner,Vector3 target){
+        this.dashSpell = dashSpell;
+        this.owner = owner;
+        this.target = target;  
+        owner.unitMover.canMove = false;
+        origin = owner.transform.position;
+        direction = (target-origin).normalized*dashSpell.maxMoveSpeed;
+        owner.GetComponent<Rigidbody>().velocity = direction;
+        GetComponent<NetworkObject>().Spawn();
+        
+    }
+    
+    // Start is called before the first frame update
+   
+
+    // Update is called once per frame
+    void Update()
+    {   owner.GetComponent<Rigidbody>().velocity = direction;
+        
+         if ((origin-owner.transform.position).magnitude>dashSpell.range)
+        {
+            owner.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            owner.unitMover.canMove=true; 
+            Debug.Log(owner.unitMover.canMove); 
+            owner.unitMover.ReachTarget();
+            Destroy(gameObject);
+        }
+        
+    }
+}
